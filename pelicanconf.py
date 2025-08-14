ed7ebea9f0c65ea642b4dc2a1e1a082c9b9c261c
ArunKumar0047 <mohandasarunkumar07@gmail.com>
2025-08-14 11:45:05 +0530

    Initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,28 @@
+AUTHOR = 'Arunkumar'
+SITENAME = 'Arunkumar'
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'India/Kolkata'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("LinkedIN", "https://www.linkedin.com/in/arunkumar-mohandas/"),
+    ("Github", "https://github.com/ArunKumar0047"),
+    ("Twitter","https://x.com/MasterofNonee__")
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
