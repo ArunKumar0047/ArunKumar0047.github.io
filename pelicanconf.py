e2c80ed1360966c43af716da09f54de5ff70de33
ArunKumar0047 <mohandasarunkumar07@gmail.com>
2025-08-26 17:35:20 +0530

    fix: Use absolute path for theme in config

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,12 +1,40 @@
-AUTHOR = 'Arunkumar'
-SITENAME = 'Arunkumar'
-SITEURL = ""
+import datetime
+import os
 
-PATH = "content"
+AUTHOR = 'Arunkumar'
+SITENAME = 'Arunkumar M'
+SITEURL = ''
+
+# Path to content, pages, and theme
+PATH = 'content'
+PAGE_PATHS = ['pages']
+THEME = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'theme')
 
 TIMEZONE = 'Europe/Rome'
-
 DEFAULT_LANG = 'en'
+
+# Add the current year to the context
+YEAR = datetime.date.today().year
+
+# URL and save paths for articles and pages
+ARTICLE_URL = 'posts/{slug}.html'
+ARTICLE_SAVE_AS = 'posts/{slug}.html'
+PAGE_URL = '{slug}.html'
+PAGE_SAVE_AS = '{slug}.html'
+
+# Define the navigation menu for the single-page layout
+MENUITEMS = (
+    ('About', '#about'),
+    ('Experience', '#experience'),
+    ('Education', '#education'),
+    ('Projects', '#projects'),
+    ('Blogs', '#blogs'),
+    ('Contact', '#contact'),
+)
+
+# We want a dedicated page for all blog posts
+DIRECT_TEMPLATES = ['index', 'blog']
+BLOG_SAVE_AS = 'blog.html'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -15,14 +43,7 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (
-    ("LinkedIN", "https://www.linkedin.com/in/arunkumar-mohandas/"),
-    ("Github", "https://github.com/ArunKumar0047"),
-    ("Twitter","https://x.com/MasterofNonee__")
-)
-
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
+RELATIVE_URLS = True
